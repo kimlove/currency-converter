2edb8ed5512203e5ce0f20a11f548912f1348341
Kim Love <kim@kimlove.com>
2024-11-21 09:42:24 +0000

    test: Updated converted total test

--- src/app/components/convertedTotal.test.tsx
+++ src/app/components/convertedTotal.test.tsx
@@ -1,11 +1,39 @@
 import { render, screen } from "@testing-library/react";
-import { ConvertedTotal } from "./convertedTotal"; // Update the import path as necessary
+import { ConvertedTotal } from "./convertedTotal";
+
+const testData = {
+  meta: {
+    code: 200,
+    disclaimer: "Usage subject to terms: https://currencybeacon.com/terms",
+  },
+  response: {
+    timestamp: 1732181033,
+    date: "2024-11-21",
+    from: "CAD",
+    to: "CUP",
+    amount: 500,
+    value: 8583.967555,
+  },
+  timestamp: 1732181033,
+  date: "2024-11-21",
+  from: "CAD",
+  to: "CUP",
+  amount: 500,
+  value: 8583.967555,
+};
 
 describe("ConvertedTotal", () => {
   it("renders the amount correctly", () => {
-    const amount = 123.45;
+    // Some weirdness with Jest and toLocaleString -- for now mock the value
+    jest.spyOn(Number.prototype, "toLocaleString").mockReturnValue("CUP 8,583.97");
 
-    render(<ConvertedTotal amount={amount} />);
-    expect(screen.getByText(amount.toString())).toBeInTheDocument();
+    render(<ConvertedTotal conversionData={testData} />);
+
+    const formattedValue = testData.response.value.toLocaleString("en-GB", {
+      style: "currency",
+      currency: testData.response.to,
+    });
+
+    expect(screen.getByText(new RegExp(`${formattedValue}`))).toBeInTheDocument();
   });
 });
